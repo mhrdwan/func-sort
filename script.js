da1dceb1a23f777247710183dee992ff120c5b2e
mhrdwan <futuremhr@gmail.com>
2023-01-30 08:21:51 +0700

    first commit

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
-// let jmlh = 10;
-// let beroperasi = 6;
+// // let jmlh = 10;
+// // let beroperasi = 6;
 
-// for (let noangkot = 1; noangkot <= jmlh; noangkot++) {
-//   if (noangkot <= beroperasi) {
-//     console.log("angkot no " + noangkot + " beroperasi");
-//   } else {
-//     console.log("angkot no " + noangkot + " mogok");
-//   }
+// // for (let noangkot = 1; noangkot <= jmlh; noangkot++) {
+// //   if (noangkot <= beroperasi) {
+// //     console.log("angkot no " + noangkot + " beroperasi");
+// //   } else if (noangkot === 8 || noangkot === 10)
+// //     console.log("angkot no " + noangkot + " lembur");
+// //   else {
+// //     console.log("angkot no " + noangkot + " mogok");
+// //   }
+// // }
+
+// // let angka = prompt("masukan angka: ");
+
+// // if (angka % 2 == 0) {
+// //   alert("angka ini adalah GENAP");
+// // } else if (angka % 2 == 1) {
+// //   alert("angka ini adalah GANJIL");
+// // } else {
+// //   alert("ini bukan angka");
+// // }
+
+// let comp = Math.random();
+// let p = prompt("gajah,semut,orang");
+
+// if (comp > 1) {
+//   comp = " gajah";
+// } else if (comp < 1 && comp < 0.067) {
+//   comp = "orang";
+// } else {
+//   comp = "semut";
 // }
 
-let angka = prompt("masukan angka: ");
+// const hasil = "";
 
-while (true) {
-  if (angka % 2 == 0) {
-    alert("angka ini adalah GENAP");
-  } else if (angka % 2 == 1) {
-    alert("angka ini adalah GANJIL");
-  } else {
-    alert("ini bukan angka");
-  }
-}
+// if (p == comp) {
+//   hasil = "seri";
+// } else if (p == "gajah") {
+//   hasil = comp == "orang" ? "meanang" : "kalah";
+// } else if (p == "orang") {
+//   hasil = comp == "gajah" ? "kalah" : "menang";
+// } else if (p == "semut") {
+//   hasil = comp == "gajah" ? "kalah" : "menang";
+// } else {
+//   hasil = "bingungkan";
+// }
+
+// alert(
+//   "kamu memilih " +
+//     p +
+//     "komputer memilih " +
+//     comp +
+//     " jadi hasilnya adalah " +
+//     hasil
+// );
+
+let angka = [1, 2, 13, 51, 61, 52, 4, 6, 78];
+
+angka.sort(function (a, b) {
+  return a - b;
+});
+console.log(angka.join(","));
